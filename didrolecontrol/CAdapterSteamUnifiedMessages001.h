d7f62ea5b258842d0c9e3ec360953701e9e23135
fire64 <fire64@yandex.ru>
2020-11-03 23:41:33 +0300

    Fix and Update EngineInterfaces

--- didrolecontrol/CAdapterSteamUnifiedMessages001.h
+++ didrolecontrol/CAdapterSteamUnifiedMessages001.h
@@ -1,21 +1,9 @@
 class CAdapterSteamUnifiedMessages001
 {
-
-	// Sends a service method (in binary serialized form) using the Steam Client.
-	// Returns a unified message handle (k_InvalidUnifiedMessageHandle if could not send the message).
-	virtual ClientUnifiedMessageHandle SendMethod( const char *pchServiceMethod, const void *pRequestBuffer, uint32 unRequestBufferSize, uint64 unContext ) = 0;
-
-	// Gets the size of the response and the EResult. Returns false if the response is not ready yet.
-	virtual bool GetMethodResponseInfo( ClientUnifiedMessageHandle hHandle, uint32 *punResponseSize, EResult *peResult ) = 0;
-
-	// Gets a response in binary serialized form (and optionally release the corresponding allocated memory).
-	virtual bool GetMethodResponseData( ClientUnifiedMessageHandle hHandle, void *pResponseBuffer, uint32 unResponseBufferSize, bool bAutoRelease ) = 0;
-
-	// Releases the message and its corresponding allocated memory.
-	virtual bool ReleaseMethod( ClientUnifiedMessageHandle hHandle ) = 0;
-
-	// Sends a service notification (in binary serialized form) using the Steam Client.
-	// Returns true if the notification was sent successfully.
-	virtual bool SendNotification( const char *pchServiceNotification, const void *pNotificationBuffer, uint32 unNotificationBufferSize ) = 0;
+	virtual unknown_ret SendMethod( char const*, void const*, uint32, uint64 ) = 0;
+	virtual unknown_ret GetMethodResponseInfo( uint64, uint32 *, EResult * ) = 0;
+	virtual unknown_ret GetMethodResponseData( uint64, void *, uint32, bool ) = 0;
+	virtual unknown_ret ReleaseMethod( uint64 ) = 0;
+	virtual unknown_ret SendNotification( char const*, void const*, uint32 ) = 0;
 };
 
